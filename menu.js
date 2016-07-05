629aff3d36406e3034b2c17b467c315b7aef8bf4
Jason Brillon <Jason62513@gmail.com>
2016-07-05 13:58:57 -0400

    accordion 2 of 2

--- menu.js
+++ menu.js
@@ -6,6 +6,9 @@ var day = date.getDate();
 var month = date.getMonth() + 1;
 var year = date.getFullYear();
 var hall = 'dewick';
+var hiddenMenuItems = ["DELI___PANINI", "FRUIT___YOGURT", "PASTA___SAUCES",
+"SAUCES_GRAVIES___TOPPINGS", "BREADS___ROLLS",
+"BRK_BREADS_PASTRIES___TOPPINGS", "CHAR_GRILL_STATIONS"]
 
 
 request.addEventListener("load", reqListener);
@@ -38,6 +41,12 @@ function handleResponse(obj) {
         sortMenuOrder(meals[i]);
     }
 
+    $("p").bind("click", function() {
+        var that = $(this).parent();
+        var newClass = (that.attr("class").includes("minus")) ? "plus" : "minus";
+        $(that).attr("class", newClass);
+    });
+
 }
 
 function parseMeal(meal) {
@@ -47,7 +56,7 @@ function parseMeal(meal) {
     for (var key in mealData) {
         if (mealData.hasOwnProperty(key)) {
             var keyName = key.replace(/[^a-zA-Z0-9+]/g, "_");
-            $(mealName).append("<ul class='" + keyName + "'><p class='minus'>" + key + "</p></ul>");
+            $(mealName).append("<ul class='" + keyName + "'><p>" + key + "</p></ul>");
 
             var dtLength = mealData[key].length;
             for (var i = 0; i < dtLength; i++) {
@@ -66,11 +75,12 @@ function sortMenuOrder(meal) {
     // bring non-hidden categories to top of menu
     $(mealID + " ul").each(function() {
         var that = this;
-        var cat = "." + $(that).attr("class");
-        if ($(cat + " li").css("display") !== "none") {
-            $(mealID).prepend($(that));
+        var cat = $(that).attr("class");
+        if (hiddenMenuItems.indexOf(cat) > -1) {
+            $("." + cat).attr("class", cat + " plus");
         } else {
-            $(cat + " p").attr("class", "plus");
+            $(mealID).prepend(that);
+            $("." + cat).attr("class", cat + " minus");
         }
     });
 
